336591215e732b1ed30b000f3f823ba94b98bb71
JohnPac <johnpac15@yahoo.com>
2022-01-03 09:21:30 -0600

    added more questions, and basic HTML write file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const jobTiltle = require("./src/temp");
+const generateHtml = require("./src/temp");
 
 
 firstQuestion = [
@@ -8,15 +8,51 @@ firstQuestion = [
         type: 'list',
         name: 'job',
         message: 'Please select a Job title',
-        choices: ['Manager', 'Engineer', 'Employee', 'Intern']
+        choices: ['Manager', 'Engineer', 'Employee', 'Intern'],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of this team member?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter thier employee ID'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter thier email address'
+    },
+    {
+        type: 'list',
+        name: 'office',
+        message: 'Please select an office number',
+        choices: ['Office 1', 'Office 2', 'Office 3', 'Office 4']
     }
 
 ]
 
+const writeFile = (fileContent) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile("./src/index.html", fileContent, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve({
+          ok: true,
+          message: "file created!",
+        });
+      });
+    });
+  };
+
 function init() { 
     inquirer.prompt(firstQuestion)
-    .then(jobData => jobTiltle(jobData))
-    .then(jobs => console.log(jobs))
+    .then(jobData => generateHtml(jobData))
+    .then(fileContent => writeFile(fileContent))
 }
 
 init()
--- src/temp.js
+++ src/temp.js
@@ -1,6 +1,6 @@
 const jobTiltle = function(jobData) {
     if(jobData.job === 'Manager'){
-        return 'Manager #1'
+        return 'Manager #1, Would you like to add another teammate????'
     }
     if(jobData.job === 'Engineer'){
         return 'Engineer #2'
@@ -12,4 +12,32 @@ const jobTiltle = function(jobData) {
         return 'Intern #4'
     }
 }
-module.exports = jobTiltle
+
+const generateHtml = function(jobData){
+    console.log(jobData)
+    return(
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+
+        <section>
+            <h2>${jobTiltle(jobData)}</h2>
+            <li>
+                <ul>${jobData.name}</ul>
+            </li>
+        </section>
+        
+    </body>
+    </html>`
+)
+}
+module.exports = generateHtml
